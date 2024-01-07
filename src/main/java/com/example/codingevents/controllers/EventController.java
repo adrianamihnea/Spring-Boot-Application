260c7fd6a7be839cb326964d51ab2e33157aef71
adrianamihnea <126603624+adrianamihnea@users.noreply.github.com>
2024-01-07 03:15:16 +0200

    Implemented coding events (chapters 1-5)

---
+++ src/main/java/com/example/codingevents/controllers/EventController.java
@@ -0,0 +1,81 @@
+package com.example.codingevents.controllers;
+
+import com.example.codingevents.data.EventCategoryRepository;
+import com.example.codingevents.data.EventRepository;
+import com.example.codingevents.models.Event;
+import com.example.codingevents.models.EventCategory;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @Autowired
+    private EventRepository eventRepository;
+    @Autowired
+    private EventCategoryRepository eventCategoryRepository;
+
+    @GetMapping
+    public String displayAllEvents(@RequestParam(required = false) Integer categoryId, Model model) {
+        if (categoryId == null) {
+            model.addAttribute("events", eventRepository.findAll());
+        }
+        else {
+            Optional<EventCategory> result = eventCategoryRepository.findById(categoryId);
+            if(result.isEmpty()) {
+                model.addAttribute("title", "Invalid categoryId");
+            }
+            else {
+                EventCategory category = result.get();
+                model.addAttribute("title", "Events in category: " + category.getName());
+                model.addAttribute("events", category.getEvents());
+            }
+        }
+        return "events/index";
+    }
+
+    // lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
+        model.addAttribute("categories", eventCategoryRepository.findAll());
+        return "events/create";
+    }
+
+    // lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@ModelAttribute @Valid Event newEvent,
+                              Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            return "events/create";
+        }
+        eventRepository.save(newEvent);
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", eventRepository.findAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+        if(eventIds != null) {
+            for(int id : eventIds) {
+                eventRepository.deleteById(id);
+            }
+        }
+        return "redirect:";
+    }
+}
---
+++ src/main/java/com/example/codingevents/models/Event.java
@@ -0,0 +1,79 @@
+package com.example.codingevents.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
+@Validated
+@Entity
+public class Event extends AbstractEntity {
+
+    @NotBlank(message = "Name is required")
+    @Size(min=3, max=50, message = "Name must be between 3 and 50 characters.")
+    private String name;
+    @Size(max = 500, message = "Description too long.")
+    private String description;
+    @NotBlank(message = "Email is required")
+    @Email(message="Invalid email. Try again.")
+    private String contactEmail;
+
+    @ManyToOne
+    @NotNull(message = "Category is required")
+    private EventCategory eventCategory;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Event(String name, String description, String contactEmail,
+                 EventCategory eventCategory) {
+        this.name = name;
+        this.description = description;
+        this.contactEmail = contactEmail;
+        this.eventCategory = eventCategory;
+    }
+
+    public Event() {};
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public EventCategory getEventCategory() {
+        return eventCategory;
+    }
+
+    public void setEventCategory(EventCategory eventCategory) {
+        this.eventCategory = eventCategory;
+    }
+}
